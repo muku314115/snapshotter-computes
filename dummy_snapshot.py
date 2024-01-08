8f9775fe979409ad94c2ca4703fbdf6bdef4af78
anomit ghosh <anomit.ghosh@gmail.com>
2024-01-08 15:32:37 +0530

    compute interface impl according to snapshotter v2 sepcs

--- dummy_snapshot.py
+++ dummy_snapshot.py
@@ -1,22 +1,23 @@
+import time
+from typing import List, Tuple, Union
 from pydantic import BaseModel
-from typing import List
-from typing import Tuple
-from typing import Union
-
+from ipfs_client.main import AsyncIPFSClient
 from redis import asyncio as aioredis
-from snapshotter.utils.callback_helpers import GenericProcessorSnapshot
+from snapshotter.utils.callback_helpers import GenericProcessor
 from snapshotter.utils.default_logger import logger
-from snapshotter.utils.models.message_models import EthTransactionReceipt
-from snapshotter.utils.models.message_models import PowerloomSnapshotProcessMessage
-from snapshotter.utils.redis.redis_keys import epoch_txs_htable
+from snapshotter.utils.models.message_models import SnapshotProcessMessage
 from snapshotter.utils.rpc import RpcHelper
+
+from .redis_keys import uniswap_v2_monitored_pairs
+from .utils.core import get_pair_reserves
+from .utils.models.message_models import EpochBaseSnapshot
 
 
 class DummySnapshotModel(BaseModel):
     dummy: str
 
 
-class DummySnapshotGenerator(GenericProcessorSnapshot):
+class DummySnapshotGenerator(GenericProcessor):
     transformation_lambdas = None
 
     def __init__(self) -> None:
@@ -25,9 +26,12 @@ class DummySnapshotGenerator(GenericProcessorSnapshot):
 
     async def compute(
         self,
-        epoch: PowerloomSnapshotProcessMessage,
-        redis_conn: aioredis.Redis,
+        msg_obj: SnapshotProcessMessage,
+        redis: aioredis.Redis,
         rpc_helper: RpcHelper,
+        anchor_rpc_helper: RpcHelper,
+        ipfs_reader: AsyncIPFSClient,
+        protocol_state_contract,
 
     ) -> Union[None, List[Tuple[str, DummySnapshotModel]]]:  
         return [
